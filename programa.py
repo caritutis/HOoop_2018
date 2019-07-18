1aebd2eebf64697b52ae5adcb931f50555da1edb
[ancapzadm] <[ancapzadm@gmail.com]>
2019-07-18 14:24:09 -0300

    se hicieron algunos cambios menores

--- programa.py
+++ programa.py
@@ -6,16 +6,30 @@ jose=cliente(2)
 jose.modificarcategoria("preferencial")
 jose.imprimir()
 Fp=FilaPreferencial()
+Fpn=FilaPreferencial()
 Fg=FilaGeneral()
 Fp.insertar(jose)
 Fg.insertar(pepe)
 print("cantidad de prefernecial=",Fp.enfila)
 #simulacion de ingreso de clientes
 #se piensa clientes pares como generales e impares como preferenciales
-for i range(1,100,1):
+maxenfila=0
+for i in range(1,100,1):
     pepe=cliente(i)
-    if (pepe.dni%2=0)
-    pepe.modificarcategoria("general")
-    else
-    pepe.modificarcategoria("preferencial")
+    if (pepe.dni%2==0):
+        pepe.modificarcategoria("general")
+        Fg.insertar(pepe)
+#se estima un tiempo de atencion de 10 minutos por cliente,
+#si pasan mas de 1h la fila se divide en dos       
+    if (pepe.dni%2==1):
+        pepe.modificarcategoria("preferencial")
+        Fp.insertar(pepe)
+    maxenfila=maxenfila+1    
+    if(maxenfila==6):    
+        Fp.abrircajanueva(maxenfila,Fpn)
+        Fpn.insertar(pepe)
+    maxenfila=0
+print("cantidad de general=",Fg.enfila)
+print("cantidad de prefernecial=",Fp.enfila)
+print("cantidad de prefernecial en la fila nueva=",Fpn.enfila)
 #este es un cle 
